602fa8246001bf3125766e62c16f58fc55e4056f
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2021-09-19 15:40:26 +1000

    Removed junk test used during debugging

--- src/test/kotlin/com/tang/intellij/test/inspections/TypeSafetyTest.kt
+++ src/test/kotlin/com/tang/intellij/test/inspections/TypeSafetyTest.kt
@@ -244,14 +244,4 @@ class TypeSafetyTest : LuaInspectionsTestBase(
     fun testVarreturn() {
         check("varreturn.lua")
     }
-
-    fun testHmm() {
-        checkByText("""
-            ---@type fun<T>(array:T[]): void
-            local fun1
-
-            ---@type fun<T>(array:T[]): void
-            local fun2 = fun1
-        """.trimIndent())
-    }
 }
