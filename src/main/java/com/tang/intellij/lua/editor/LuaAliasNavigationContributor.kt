e425125ccc63bb9b66e8f3a1a93ed025f576efab
Benjamin Dobell <git@benjamin.dobell.email>
2023-03-25 23:39:36 +1100

    Now including aliases in IntelliJ's goto class action UI

--- src/main/java/com/tang/intellij/lua/editor/LuaAliasNavigationContributor.kt
+++ src/main/java/com/tang/intellij/lua/editor/LuaAliasNavigationContributor.kt
@@ -7,13 +7,13 @@ import com.intellij.util.Processor
 import com.intellij.util.containers.ContainerUtil
 import com.intellij.util.indexing.FindSymbolParameters
 import com.intellij.util.indexing.IdFilter
-import com.tang.intellij.lua.stubs.index.LuaClassIndex
+import com.tang.intellij.lua.stubs.index.LuaAliasIndex
 
-class LuaClassNavigationContributor : ChooseByNameContributorEx {
+class LuaAliasNavigationContributor : ChooseByNameContributorEx {
     override fun processNames(processor: Processor<in String>,
                               scope: GlobalSearchScope,
                               filter: IdFilter?) {
-        ContainerUtil.process(LuaClassIndex.instance.getAllKeys(scope.project), processor)
+        ContainerUtil.process(LuaAliasIndex.instance.getAllKeys(scope.project), processor)
     }
 
     override fun processElementsWithName(name: String,
@@ -21,8 +21,8 @@ class LuaClassNavigationContributor : ChooseByNameContributorEx {
                                          parameters: FindSymbolParameters) {
         val project = parameters.project
         val searchScope = parameters.searchScope
-        LuaClassIndex.instance.processAllKeys(project) {
-            ContainerUtil.process(LuaClassIndex.instance.get(it, project, searchScope), processor)
+        LuaAliasIndex.instance.processAllKeys(project) {
+            ContainerUtil.process(LuaAliasIndex.instance.get(it, project, searchScope), processor)
         }
     }
 }
